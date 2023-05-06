c88527541f505d11e5c986dd80353f7ae334a57e
LibertyReman <libertyreman@gmail.com>
2023-05-06 23:36:29 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let { src, watch, dest, parallel } = require('gulp');
+let sass = require('gulp-dart-sass')(require('sass'));  // DartSass build
+let plumber = require('gulp-plumber');                  // gulpタスクでエラー発生時に強制終了するのを防ぐ
+let browserSync = require('browser-sync');              // ローカルサーバーを立ててファイル監視し変更があったら自動的にブラウザ更新する
+
+
+const sassBuild = (done) => {
+  src('./scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(dest('./css'));
+  done();
+};
+
+const watchFiles = () => {
+  watch('./scss/*.scss', sassBuild);
+  watch(['./css/*.css', './*.html'], bsReload);
+};
+
+const startBrowserSync = () => {
+  browserSync({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+}
+
+const bsReload = (done) => {
+  browserSync.reload();
+  done();
+};
+
+
+exports.default = parallel(startBrowserSync, watchFiles);
+exports.build = sassBuild;
+
